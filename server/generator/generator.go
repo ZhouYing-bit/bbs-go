6c98a7f3455121830a3813ec996f276fba70c259
joey <1011875777@qq.com>
2022-04-22 16:29:40 +0800

    编译通过

--- server/generator/generator.go
+++ server/generator/generator.go
@@ -1,11 +1,11 @@
 package main
 
-import (
-	"github.com/mlogclub/codegen"
-
-	"bbs-go/model"
-)
-
-func main() {
-	codegen.Generate("./", "bbs-go", codegen.GetGenerateStruct(&model.StickyTopic{}))
-}
+//import (
+//	"github.com/mlogclub/codegen"
+//
+//	"bbs-go/model"
+//)
+//
+//func main() {
+//	codegen.Generate("./", "bbs-go", codegen.GetGenerateStruct(&model.StickyTopic{}))
+//}
